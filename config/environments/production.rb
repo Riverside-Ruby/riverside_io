55c10fed936bbe00f57bde21b5bb67acf697597b
Matt Goatcher <matt@angcrest.net>
2016-11-09 20:07:49 -0800

    Fix email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'riverside-io.herokuapp.com' }
+  config.action_mailer.asset_host = "http://riverside-io.herokuapp.com"
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
