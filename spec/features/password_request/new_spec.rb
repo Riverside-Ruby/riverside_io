843df3a33d3e3bd4a2a51fe71447ac7d1bc8f931
Iggy <igoririanto@rocketmail.com>
2017-02-15 16:13:39 -0800

    finish basic layout edit

--- spec/features/password_request/new_spec.rb
+++ spec/features/password_request/new_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "New Password Request", type: :feature do
   it "allows a guest to request a new password" do
     visit root_path
     click_link "Request Wifi password"
-    fill_in "Email", with: "user@example.com"
+    fill_in "email", with: "user@example.com"
 
     expect { click_button "Submit" }.to change(PasswordRequest, :count).by(1)
 
